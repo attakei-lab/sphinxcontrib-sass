34cca5ae3d9e406236049d3fedf4413c0aff3724
Kazuya Takei <myself@attakei.net>
2021-04-19 16:40:03 +0900

    Add docs as example

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# -- Path setup --------------------------------------------------------------
+
+# -- Project information -----------------------------------------------------
+project = 'sphinxcontrib-sass example'
+copyright = '2021, Kazuya Takei'
+author = 'Kazuya Takei'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinxcontrib.sass',
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
+
+sass_out_dir = '_static/css'
+sass_targets = {
+    '_sass/custom.scss': '_static/css/custom.css',
+}
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. sphinxcontrib-sass example documentation master file, created by
+   sphinx-quickstart on Mon Apr 19 16:32:28 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinxcontrib-sass example's documentation!
+======================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
